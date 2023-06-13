b878e6e38706273b035aa3bd8f23ddd01ff1d87f
florence wangechi <florencewangechim@gmail.com>
2023-06-13 12:36:58 +0300

    orm

--- lib/dog.py
+++ lib/dog.py
@@ -5,4 +5,23 @@ CURSOR = CONN.cursor()
 
 class Dog:
     
-    pass
+    all = []
+    
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS dogs(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+            );
+        """ 
+
+        cls.all = CURSOR.execute(sql)
+
+        
+
